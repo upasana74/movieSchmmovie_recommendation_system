ad1dddbb4c7d719c8531d1095f5c583c9c7e5c86
Upasana Thakuria <upasanathakuria@gmail.com>
2022-12-17 16:44:10 -0500

    minor omdb changes

--- omdb_scrapping.py
+++ omdb_scrapping.py
@@ -1,9 +1,10 @@
 import requests
 import json
 api_key = "6a4ba2a0"
+# movie_id = '3896198'
 
 def scraping_from_omdb_api(): # returns dict
-    url = f"http://www.omdbapi.com/?i=tt3896198&apikey={api_key}"
+    url = f"http://www.omdbapi.com/?i=tt{movie_id}&apikey={api_key}"
     response = requests.get(url)
     response_json=json.loads(response.text)
     print(response_json)
