1ffa94c84515641c072edf0f986ed0e7128787b3
Upasana Thakuria <upasanathakuria@gmail.com>
2022-12-17 16:45:38 -0500

    omdb fixed

--- omdb_scrapping.py
+++ omdb_scrapping.py
@@ -3,7 +3,7 @@ import json
 api_key = "6a4ba2a0"
 # movie_id = '3896198'
 
-def scraping_from_omdb_api(): # returns dict
+def scraping_from_omdb_api(movie_id): # returns dict
     url = f"http://www.omdbapi.com/?i=tt{movie_id}&apikey={api_key}"
     response = requests.get(url)
     response_json=json.loads(response.text)
